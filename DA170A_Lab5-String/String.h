befc8e0fb566adb5329195be27135afd2f868d06
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2018-01-05 19:53:21 +0100

    Const correct I think, Inline, prolly rollback semantics

--- DA170A_Lab5-String/String.h
+++ DA170A_Lab5-String/String.h
@@ -12,38 +12,67 @@ public:
 	String& operator=(String &&rhs);
 	~String();
 
-	explicit operator bool();
-	char& At(size_t i);
-	const char& String::At(const size_t i) const;
-	char& operator[](size_t i);
-	const char& operator[](size_t i) const;
-	const char* Data() const;
-
-	int Size() const;
 	void Reserve(size_t);
-	int Capacity() const;
 	void Shrink_to_fit();
-
 	void Push_back(char c);
 	void Resize(size_t n);
-
 	String& operator +=(const String& rhs);
 	String String::operator+(const String &rhs) const;
 
 	friend bool operator==(const String &lhs, const String &rhs);
 	friend bool operator !=(const String &lhs, const String &rhs);
+	friend std::ostream& operator<<(std::ostream& cout, String& rhs);
 
-	friend std::ostream& operator<<(std::ostream& cout, String& rhs)
+	inline char& String::At(const size_t i)
 	{
-		for (size_t i = 0; i < rhs.size; ++i)
-		{
-			cout << rhs[i];
-		}
-		return cout;
+		if (i < 0 || capacity <= i)
+		{ throw std::out_of_range("Index out of bounds."); }
+		else 
+		{ return letters[i]; }
 	}
+
+	inline const char& String::At(const size_t i) const
+	{
+		if (i < 0 || capacity <= i) 
+		{ throw std::out_of_range("Index out of bounds."); }
+		else 
+		{ return letters[i]; }
+	}
+
+	inline operator bool() 
+	{ return capacity != 0; }
+
+	inline char& String::operator[](const size_t i) 
+	{ return letters[i]; }
+
+	inline const char& String::operator[](const size_t i) const 
+	{ return letters[i]; }
+
+	inline char* String::Data() 
+	{ return letters; }
+
+	inline const char* String::Data() const 
+	{ return letters; }
+
+	inline int Capacity() 
+	{ return capacity; }
+
+	inline int Capacity() const 
+	{ return capacity; }
+
+	inline int String::Size() 
+	{ return size; }
+
+	inline const int String::Size() const 
+	{ return size; }
 
 private:
 	char *letters;
 	int size;
 	int capacity;
+
+	inline bool Invariant() const
+	{
+		return capacity >= size && (letters == nullptr || letters[size] == '\0');
+	}
 };
